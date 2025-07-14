88286fcdbe2e189fb3814cb87395e7ef31b31b4c
DeweQ <66887312+DeweQ@users.noreply.github.com>
2025-07-14 09:21:53 +0300

    Update removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function( arr, ...argsToRemove) {
+    for (let arg in argsToRemove) {
+        if (arr.indexOf(arg)>=0)
+            arr.splice(arr.indexOf(arg),1);
+    }
 };
 
 // Do not edit below this line
