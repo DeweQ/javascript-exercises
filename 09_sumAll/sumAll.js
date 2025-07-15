54955499fd1ed3a993bc105066fc1dabf08a6980
DeweQ <thedeweq@yandex.ru>
2025-07-15 06:28:30 +0300

    Update sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0)
+        return "ERROR";
+    if (!(typeof(start) == typeof(end) == typeof(123)))
+        return "ERROR";
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let result = 0;
+    for (let i = start; i <= end; i++)
+        result += i;
+    return result;
 };
 
+sumAll(123, 1);
 // Do not edit below this line
 module.exports = sumAll;
