cd3c9dea05a80ebe6f5d0ef4d800ea11680ec2a7
DeweQ <thedeweq@yandex.ru>
2025-07-22 07:31:31 +0300

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let result = str.toLowerCase();
+  const punctuation = [",", ".", "!", " "];
+  for (let sym of punctuation)
+    if (result.indexOf(sym)>=0)
+      result = result.replaceAll(sym,"");
+  
+  return result == result.split("").reverse().join("");
 };
+//console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
