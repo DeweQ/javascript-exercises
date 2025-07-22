34145b4ca7179d2652f8acd4ac5bb6419ec68b9b
DeweQ <thedeweq@yandex.ru>
2025-07-22 11:20:15 +0300

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  people.sort((a,b) => {
+    let ageA = "yearOfDeath" in a ? a.yearOfDeath - a.yearOfBirth: new Date().getFullYear() - a.yearOfBirth;
+    let ageB = "yearOfDeath" in b ? b.yearOfDeath - b.yearOfBirth: new Date().getFullYear() - b.yearOfBirth;
+    return ageB - ageA;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
