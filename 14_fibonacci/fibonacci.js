76026ad6441b7c0560377220dec2862791836e94
DeweQ <thedeweq@yandex.ru>
2025-07-22 08:49:59 +0300

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  index = parseInt(index);
+  let fibbonaciArr =[0,1,1];
+  if (index < 0)
+    return "OOPS";
+  for(let i = 3; i <= index; i++){
+    fibbonaciArr[i] = fibbonaciArr[i-1] + fibbonaciArr[i-2];
+	}
+	return fibbonaciArr[index];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
